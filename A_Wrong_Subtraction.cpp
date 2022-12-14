a8facabc090df515c5157cdb3504f9a904742531
Jay Makhija <100950319+jay13makhija@users.noreply.github.com>
2022-12-14 11:30:43 +0530

    14/12/22

---
+++ A_Wrong_Subtraction.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+    for(int i=0; i<b; i++)
+    {
+        if(a%10 != 0)
+        {
+            a -= 1;
+        }
+        else
+        {
+            a/= 10;
+        }
+    }
+
+    cout << a;
+
+return 0;
+}
